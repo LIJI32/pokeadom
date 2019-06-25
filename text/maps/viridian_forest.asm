184016de3920323ee73a2087815cf26f3dcd99e3
EgaoDaYo <51708131+EgaoDaYo@users.noreply.github.com>
2019-06-25 17:10:30 +0300

    Update viridian_forest.asm

--- text/maps/viridian_forest.asm
+++ text/maps/viridian_forest.asm
@@ -1,4 +1,4 @@
-_ViridianForestText1::
+_ViridianForestText1:: ; 8031d (20:431d)
 	text "באתי הנה עם"
 	line "!כמה חברים"
 
@@ -6,53 +6,53 @@ _ViridianForestText1::
 	line "!קרבות פוקימונים"
 	done
 
-_ViridianForestBattleText1::
+_ViridianForestBattleText1:: ; 80359 (20:4359)
 	text "היי! יש לך"
 	line "!פוקימונים"
 	cont "קדימה! בוא ניתן"
 	cont "!להם להילחם"
 	done
 
-_ViridianForestEndBattleText1::
+_ViridianForestEndBattleText1:: ; 80387 (20:4387)
 	text "!לא"
 	line "קאטרפי לא יכול"
 	cont "!לעמוד בזה"
 	prompt
 
-_ViridianFrstAfterBattleText1::
+_ViridianFrstAfterBattleText1:: ; 803a2 (20:43a2)
 	text "ששש! אתה תרחיק"
 	line "!מפה את החרקים"
 	done
 
-_ViridianForestBattleText2::
+_ViridianForestBattleText2:: ; 803c3 (20:43c3)
 	text "יו! אתה לא יכול"
 	line "להשתפן אם אתה"
 	cont "!מאמן פוקימונים"
 	done
 
-_ViridianForestEndBattleText2::
+_ViridianForestEndBattleText2:: ; 803f2 (20:43f2)
 	text "?מה"
 	line "נגמרו לי"
 	cont "!הפוקימונים"
 	prompt
 
-_ViridianFrstAfterBattleText2::
+_ViridianFrstAfterBattleText2:: ; 8040b (20:440b)
 	text "לעזאזל! אני"
 	line "אתפוס פוקימונים"
 	cont "!יותר חזקים"
 	done
 
-_ViridianForestBattleText3::
+_ViridianForestBattleText3:: ; 80438 (20:4438)
 	text "!היי, חכה"
 	line "?לאן אתה ממהר"
 	done
 
-_ViridianForestEndBattleText3::
+_ViridianForestEndBattleText3:: ; 80458 (20:4458)
 	text "!אני נכנע"
 	line "!אתה טוב בזה"
 	prompt
 
-_ViridianFrstAfterBattleText3::
+_ViridianFrstAfterBattleText3:: ; 80475 (20:4475)
 	text "לפעמים אתה"
 	line "יכול למצוא"
 	cont "!חפצים על האדמה"
@@ -61,7 +61,7 @@ _ViridianFrstAfterBattleText3::
 	line "!משהו שהפלתי"
 	done
 
-_ViridianForestText8::
+_ViridianForestText8:: ; 804c7 (20:44c7)
 	text "נגמרו לי"
 	line "הפוכדורים בשביל"
 	cont "!ללכוד פוקימונים"
@@ -70,7 +70,7 @@ _ViridianForestText8::
 	line "!כמה עודפים"
 	done
 
-_ViridianForestText9::
+_ViridianForestText9:: ; 8050a (20:450a)
 	text "עצות למאמן"
 
 	para "אם אתה רוצה"
@@ -80,14 +80,14 @@ _ViridianForestText9::
 	cont "!העשביים"
 	done
 
-_ViridianForestText10::
+_ViridianForestText10:: ; 80553 (20:4553)
 	text "כנגד רעל, השתמש"
 	line "בנוגדן! ניתן"
 	cont "להשיג אותו"
 	cont "!בפוקימרקט"
 	done
 
-_ViridianForestText11::
+_ViridianForestText11:: ; 80584 (20:4584)
 	text "עצות למאמן"
 
 	para "צור קשר עם"
@@ -97,7 +97,7 @@ _ViridianForestText11::
 	cont "!הפוקידע שלך"
 	done
 
-_ViridianForestText12::
+_ViridianForestText12:: ; 805c6 (20:45c6)
 	text "עצות למאמן"
 
 	para "אסור לגנוב"
@@ -108,7 +108,7 @@ _ViridianForestText12::
 	cont "!פראיים"
 	done
 
-_ViridianForestText13::
+_ViridianForestText13:: ; 80613 (20:4613)
 	text "עצות למאמן"
 
 	para "החלש את"
@@ -121,7 +121,7 @@ _ViridianForestText13::
 	cont "!לברוח"
 	done
 
-_ViridianForestText14::
+_ViridianForestText14:: ; 80667 (20:4667)
 	text "יציאה"
 	line "יער ורידיאן"
 	cont "העיר פיוטר"
